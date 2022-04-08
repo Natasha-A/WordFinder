058ce7d441891264ba2f7b478574c7d053dc6a55
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2022-04-07 21:04:08 -0400

    addded subcomponets

--- finalproject-app/src/components/Homepage.js
+++ finalproject-app/src/components/Homepage.js
@@ -1,1 +1,17 @@
 import React from "react";
+
+function App() {
+
+  // Homepage
+  <div className="...">
+ <Wordofday></Wordofday>
+  <Boomark></Boomark>
+
+  // Router - nav bar
+  <Searchbar> // press enter
+    <router>
+      <Results />
+    </router>
+  </Searchbar>
+  </div>
+}
---
+++ finalproject-app/src/components/Results.js
@@ -0,0 +1,13 @@
+// definitions comp 
+
+function Definitions() {
+  // https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=your-api-key
+  // fetch calls 
+  // bootstrap 
+
+}
+
+// thesaraus comp
+function thesaraus() {
+  
+}
