29112fa4db1826809f4b308b674d06918a05bff4
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2022-04-10 02:52:17 -0400

    Dictionary axios fetch api request added

--- finalproject-app/src/components/Homepage.js
+++ finalproject-app/src/components/Homepage.js
@@ -1,8 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 function App() {
+  const dictKey = 'db581e8e-4d30-455a-a8b9-d1de2f4dc84c';
+  const thesKey = '1b9cfecf-a383-4f09-93e2-745e7003f274';
+
+  const dictionaryAPI = async() => {
+    try {
+      const data=await axios.get('https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=' + dictKey);
+      console.log(data);
+    } catch(error) {
+      console.log(error);
+    }
+  };
+
+  // called every time our variable changes
+  useEffect(() => {
+    dictionaryAPI();
+  }, []);
 
   // Homepage
+  /*
   <div className="...">
  <Wordofday></Wordofday>
   <Boomark></Boomark>
@@ -13,5 +31,7 @@ function App() {
       <Results />
     </router>
   </Searchbar>
-  </div>
+  </div>*/
 }
+
+export default App;
