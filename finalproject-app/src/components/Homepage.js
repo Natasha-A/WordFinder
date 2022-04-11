0fb91e9302f2291cf975f9bb68b8e6e618bec23a
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2022-04-11 10:04:07 -0400

    rename 'Defintions.js' to 'Dictionary.js'

--- finalproject-app/src/components/Homepage.js
+++ finalproject-app/src/components/Homepage.js
@@ -2,23 +2,11 @@ import axios from "axios";
 import React, { useEffect } from "react";
 
 function App() {
-  const dictKey = 'db581e8e-4d30-455a-a8b9-d1de2f4dc84c';
-  const thesKey = '1b9cfecf-a383-4f09-93e2-745e7003f274';
-
-  const dictionaryAPI = async() => {
-    try {
-      const data=await axios.get('https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=' + dictKey);
-      console.log(data);
-    } catch(error) {
-      console.log(error);
-    }
-  };
-
-  // called every time our variable changes
-  useEffect(() => {
-    dictionaryAPI();
-  }, []);
-
+  return (
+    <div>
+      
+    </div>
+  );
 }
 
 export default App;
--- finalproject-app/src/index.js
+++ finalproject-app/src/index.js
@@ -1,10 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Homepage.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/*Comment here added*/}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
